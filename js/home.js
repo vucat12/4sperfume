b3095ab75530e03c4e23b27bde9cbac120435e95
Your Name <you@example.com>
2020-09-29 08:29:11 +0700

    pull-data

--- js/home.js
+++ js/home.js
@@ -1,12 +1,17 @@
 const parallaxHeader = document.getElementById("header");
-window.addEventListener("scroll",function(){
+window.addEventListener("scroll", function () {
     let offset = window.pageYOffset;
     parallaxHeader.style.backgroundPositionY = offset * 0.7 + "px";
 })
 
 const openMenu = document.getElementById("menu");
-openMenu.addEventListener("click", function(){
-    var backgroundOfMenu = document.getElementById("backgroundOfMenu");
+openMenu.addEventListener("click", function () {
+    let backgroundOfMenu = document.getElementById("backgroundOfMenu");
     backgroundOfMenu.style.opacity = "1";
+    if (openMenu.innerText === "CLOSE") {
+        openMenu.innerText = "MENU";
+        backgroundOfMenu.style.opacity = "0";
+    } else {
+        openMenu.innerText = "CLOSE";
+    }
 })
-
